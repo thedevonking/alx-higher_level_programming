a45190b45e63e1028c4acfad2bc0fffb03ec38d3
D'von King <117774846+thedevonking@users.noreply.github.com>
2023-03-16 17:00:10 +0100

    Update 13-insert_number.c

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 
 /**
- * insert_node - function that inserts numbers into a sorted singly linked list
+ * insert_node - function that inserts number into a sorted singly linked list
  * @head: head of the linked list
  * @number: data of the new node
- * Return: address of the node of NULL if fails
+ * Return: address of new node or NULL if fails
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new, *aux1 - *head, aux2 = *head, aux4 = *head;
+	listint_t *new, *aux1 = *head, *aux2 = *head, *aux3 = *head;
 	unsigned int i = 0, idx = 0;
 
 	new = malloc(sizeof(listint_t));
@@ -19,25 +19,24 @@ listint_t *insert_node(listint_t **head, int number)
 	new->next = NULL;
 
 	if (*head == NULL)
-	{
-		new->next = *head;
+	{	new->next = *head;
 		*head = new;
-		return (new);		}
+		return (new);	}
 
 	while (aux1->next)
-	{      idx++;
+	{	idx++;
 		aux3 = aux3->next;
-		if (aux1->n number)
+		if (aux1->n > number)
 		{	idx--;
-			break;		}
+			break;	}
 		else if (aux1->next->next == NULL && aux3->n < number)
 		{	idx++;
-			break;		}
-		aux1 = aux1->next;		}
+			break;	}
+		aux1 = aux1->next;	}
 	if (idx == 0)
 	{	new->next = *head;
 		*head = new;
-		return (new);		}
+		return (new);	}
 	while (aux2 != NULL && i < idx - 1)
 	{	aux2 = aux2->next;
 		i++;	}
