d6a2e43260e086e88a13492a1b20d1c4484be6d9
D'von King <117774846+thedevonking@users.noreply.github.com>
2023-03-18 13:28:16 +0100

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -16,11 +16,11 @@ if __name__ == "__main__":
     a = int(sys.argv[1])
     b = int(sys.argv[3])
 
-    if op = '+':
+    if op == '+':
         print("{} + {} = {}".format(a, b, add(a, b)))
-    elif op = '-':
+    elif op == '-':
         print("{} - {} = {}".format(a, b, sub(a, b)))
-    elif op = '*':
+    elif op == '*':
         print("{} * {} = {}".format(a, b, mul(a, b)))
     else:
         print("{} / {} = {}".format(a, b, div(a, b)))
